5408e597a3bd513996fcd0142967e7c886ea51bc
Andre Meireles <andre.meireles@relational.ai>
2023-05-17 16:58:04 -0300

    Setup step - Check local and remote migrations and generate the migration status

--- migrator/migration_control.go
+++ migrator/migration_control.go
@@ -18,26 +18,46 @@ func GetMigrationStatus(migrationScripts map[int]MigrationScript) (*MigrationSta
 		return nil, fmt.Errorf("migrator.getMigrationStatus: %w", err)
 	}
 
+	noMigrations := &MigrationStatus{
+		CurrentMigrationId: -1,
+		NextMigrations:     migrationScripts,
+	}
 	if len(tranx.Problems) > 0 {
 		problems := tranx.Problems
 		for _, iProb := range problems {
-			problem, notErr := iProb.(rai.ClientProblem)
-			if !notErr {
+			problem, typeInferenceSucceed := iProb.(rai.ClientProblem)
+			if !typeInferenceSucceed {
 				return nil, fmt.Errorf("migrator.getMigrationStatus: type inference failed for rai.ClientProblem")
 			}
-			if notErr && problem.ErrorCode == "UNDEFINED" {
-				return &MigrationStatus{
-					CurrentMigrationId: -1,
-					NextMigrations:     migrationScripts,
-				}, nil
+			if typeInferenceSucceed && problem.ErrorCode == "UNDEFINED" {
+				return noMigrations, nil
 			} else {
 				return nil, fmt.Errorf("migrator.getMigrationStatus: unknown error - %v", problem)
 			}
 		}
 	}
-	return nil, nil
+
+	if len(tranx.Results) == 0 {
+		return noMigrations, nil
+	}
+
+	lastMigrationID := int(tranx.Results[0].Table[0].(float64))
+	nextMigrations := make(map[int]MigrationScript)
+
+	for id, migrationScript := range migrationScripts {
+		if id > lastMigrationID {
+			nextMigrations[id] = migrationScript
+		}
+	}
+
+	migrationStatus := MigrationStatus{
+		CurrentMigrationId: lastMigrationID,
+		NextMigrations:     nextMigrations,
+	}
+
+	return &migrationStatus, nil
 }
 
 const CurrentMigrationQuery = `//beginrel
-	def output = raiway_migrations
+	def output = max[id: raiway_migrations(_, id, _)]
 //endrel`
